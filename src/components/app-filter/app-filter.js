f1dcdf13d450ccb4c4fa60e3beee099f1de558a1
Roman <kntroman@gmail.com>
2023-06-08 00:35:25 +0600

    add filter func

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,25 +1,45 @@
+import { Component } from 'react';
+
 import './app-filter.css';
 
-const AppFilter = () => {
-  return (
-    <div className='btn-group'>
-      <button 
-        className='btn btn-light'
-        type='button'>
-          Все сотрудники
-      </button>
-      <button 
-        className='btn btn-outline-light'
-        type='button'>
-          Сотрудники на повышение
-      </button>
-      <button 
-        className='btn btn-outline-light'
-        type='button'>
-          З/П больше 1000$
-      </button>
-    </div>
-  )
+class AppFilter extends Component  {
+  constructor (props) {
+    super(props);
+    this.state = {
+      filter: ''
+    }
+  }
+  onFilterSelect = (filter) => {
+    this.setState({filter});
+    this.props.onFilterSelect(filter);
+  }
+
+  render() {
+    return (
+      <div className='btn-group'>
+        <button 
+          className='btn btn-light'
+          type='button'
+          onClick={() => this.onFilterSelect('all')}
+          >
+            Все сотрудники
+        </button>
+        <button 
+          className='btn btn-outline-light'
+          type='button'
+          onClick={() => this.onFilterSelect('rise')}
+          >
+            Сотрудники на повышение
+        </button>
+        <button 
+          className='btn btn-outline-light'
+          type='button'
+          onClick={() => this.onFilterSelect('moreThen1000')}
+          >
+            З/П больше 1000$
+        </button>
+      </div>
+    )}
 }
 
 export default AppFilter;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,9 @@ class App extends Component {
         {name:'Mike B.',salary: 1000,increase: true, rise: false, id:2},
         {name:'Brit A.',salary: 3000,increase: true, rise: false, id:3},
         {name:'Rouen F.',salary: 1500,increase: false, rise: true, id:4},
-      ]
+      ],
+      term: '',
+      filter: ''
     }
     this.maxId = 5;
   }
@@ -84,17 +86,47 @@ class App extends Component {
     }))
   }
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter(item => {
+      return item.name.indexOf(term) > -1
+    })
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({term});
+  }
+
+  filterPost = (items, filter) => {
+    switch(filter) {
+      case 'rise':
+        return items.filter(item => item.rise)
+      case 'moreThen1000':
+        return items.filter(item => item.salary > 1000)
+      default:
+        return items;
+    }
+  }
+
+  onFilterSelect = (filter) => {
+    this.setState({filter})
+  }
+
   render() {
+    const {data, term, filter} = this.state;
+    const visibaleDate = this.filterPost(this.searchEmp(data, term),filter);
     return (
       <div className="app">
         <AppInfo
           data={this.state.data}/>
         <div className="search-panel">
-          <SearchPanel/>
-          <AppFilter/>
+          <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+          <AppFilter onFilterSelect={this.onFilterSelect}/>
         </div>
         <EmployeesList 
-          data={this.state.data}
+          data={visibaleDate}
           onDelete={this.deleteItem}
           onToggleIncrease={this.onToggleIncrease}
           onToggleRise={this.onToggleRise}/>
